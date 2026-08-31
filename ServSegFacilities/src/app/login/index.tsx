200e8fd5cfea4e237cdd405f145a52192bf56940
KAUÊ SERGIO DA CONCEIÇÃO SILVA CAMPOS <23969368839@info.local>
2026-08-31 10:56:30 -0300

    :heavy_plus_sign: build: adicionando biometria

--- ServSegFacilities/src/app/login/index.tsx
+++ ServSegFacilities/src/app/login/index.tsx
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
-import { Alert, Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, Image, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { H1, H2, Input } from "../../constants/theme";
 
 import Logo from '../../../assets/imgs/ServSeg Escuro.svg'
 import { useAuth } from "../../contexts/AuthContext";
 import { useRouter } from "expo-router";
 import { autenticacaoService } from "../../services/autenticacaoService";
+import { useBiomet } from "../../hooks/useBiomet"
 
-export default function Login () {
+export default function Login() {
     const { login } = useAuth();
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const router = useRouter();
-    
-    async function acessar(){
+    const { ativarBiometria } = useBiomet();
+
+    async function acessar() {
         router.push("/") //! LEMBRAR DE TROCAR DEPOIS
         const emailDigitado = email.trim();
         const senhaDigitada = senha.trim();
 
-        if(emailDigitado || !senhaDigitada){
+        if (emailDigitado || !senhaDigitada) {
             Alert.alert("Login inválido!", "Preencha o e-mail e a senha corretamente.")
             return;
         }
 
-        try{
-            await autenticacaoService.login({email: emailDigitado, senha: senhaDigitada})
+        try {
+            await autenticacaoService.login({ email: emailDigitado, senha: senhaDigitada })
             router.replace("/") //! LEMBRAR DE TROCAR DEPOIS
-        } catch(error){
+        } catch (error) {
             Alert.alert("Login inválido!", "E-mail ou senha inválidos.")
         }
     }
     return (
-        <View style={{ flex: 1, position: 'relative'}}>
+        <View style={{ flex: 1, position: 'relative' }}>
             <Image source={require('../../../assets/imgs/Mapa1.png')}
-            style={{ width: '100%', height: 375, position: 'absolute' }} resizeMode="contain"/>
+                style={{ width: '100%', height: 375, position: 'absolute' }} resizeMode="contain" />
             <View style={styles.logo}>
                 <Logo width={150} />
                 <Text style={H1}>ServSeg Facilities</Text>
@@ -41,17 +43,20 @@ export default function Login () {
             <View>
                 <Text style={H2}>E-Mail</Text>
                 <TextInput style={Input} placeholder="email@email.com"
-                value={email} onChangeText={setEmail} keyboardType="email-address"/>
+                    value={email} onChangeText={setEmail} keyboardType="email-address" />
                 <Text style={H2}>Senha</Text>
                 <TextInput style={Input} placeholder="********"
-                value={senha} onChangeText={setSenha}/>
+                    value={senha} onChangeText={setSenha} />
             </View>
+            <Pressable onPress={ativarBiometria}>
+                <Text>Biometria.com</Text>
+            </Pressable>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    logo:{
+    logo: {
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 125
